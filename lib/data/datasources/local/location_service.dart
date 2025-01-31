6310861c1d93b97e72d9bc67e5451ab79a23f735
NitionVR <kmaganyane83@gmail.com>
2025-01-31 08:14:10 +0200

    Improve the current location tracking logic

--- lib/data/datasources/local/location_service.dart
+++ lib/data/datasources/local/location_service.dart
@@ -13,8 +13,18 @@ class LocationService{
     return await _location.requestPermission();
   }
 
-  Future<LocationData> getCurrentLocation() async {
-    return await _location.getLocation();
+  Future<LocationData?> getCurrentLocation() async {
+    try {
+      await _location.changeSettings(
+        accuracy: LocationAccuracy.high,
+        interval: 1000,
+      );
+
+      return await _location.getLocation();
+    } catch (e) {
+      print('Error getting location: $e');
+      return null;
+    }
   }
 
   Stream<LocationData> get locationStream => _location.onLocationChanged;
